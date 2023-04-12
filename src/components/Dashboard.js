07772f6707ed9ef392db9cfe84ebf7af1f33f6e7
Abhijit Taware <abhijittaware85@gmail.com>
2023-04-12 08:03:30 +0530

    Initial Commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import CustomTable from "./table/CustomTable";
 import "./Dashboard.css";
-import { searchForUsers } from "./search/SearchUser";
 
 
 const  Dashboard=()=> {
@@ -33,6 +32,20 @@ const  Dashboard=()=> {
       });
     }
 
+    const searchForUsers=(search,userData)=>{
+      return userData.map((user) => {
+          if (
+            user.name.toLowerCase().includes(search.toLowerCase()) ||
+            user.email.toLowerCase().includes(search.toLowerCase()) ||
+            user.role.toLowerCase().includes(search.toLowerCase())
+          ) {
+             user.visibility = true;
+             return user;
+          }
+          user.visibility = false;
+          return user;
+        });
+  }
     const selectAll = (e) => {
       // console.log(e.target.checked);
       const UserIds = resData
